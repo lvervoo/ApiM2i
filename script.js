9041579fff85298a3f55d87bbb866414756cb0c4
lvervoo <vervoort.loic@gmail.com>
2024-01-12 10:34:36 +0100

    tacheAcommplie

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ var dropper = document.querySelector("#dropper")
 dropper.addEventListener('dragover', (event) => {
     event.preventDefault()
 })
+
+dropper.addEventListener('dragenter', (event) => {
+    dropper.style.backgroundColor = 'white'
+})
+
+dropper.addEventListener('dragleave', (event) => {
+    dropper.style.backgroundColor = 'dodgerblue'
+})
+
+draggable.addEventListener('dragstart', (event) => {
+    event.dataTransfer.setData('text/plain', draggable.innerHTML)
+})
+
+dropper.addEventListener('drop', (event) => {
+    const data = event.dataTransfer.getData('text/plain')
+    dropper.innerHTML = `${data}  ${dropper.innerHTML}`
+    dropper.style.backgroundColor = 'dodgerblue'
+})
